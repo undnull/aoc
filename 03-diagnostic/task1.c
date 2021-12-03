87536da9331b217431ec85f48239778c9e0138c0
undnull <41549137+undnull@users.noreply.github.com>
2021-12-03 23:10:52 +0500

    Day 3: Use INPUT_BITS for the task1

--- 03-diagnostic/task1.c
+++ 03-diagnostic/task1.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define INPUT_BITS 12
+
 int main(void)
 {
     int i;
     FILE *fp;
     unsigned int bit, count, gamma, eps;
-    unsigned int bits[12] = { 0 };
-    char line[16] = { 0 };
+    unsigned int bits[INPUT_BITS] = { 0 };
+    char line[INPUT_BITS * 2] = { 0 };
 
     fp = fopen("./input", "r");
     count = gamma = eps = 0;
     memset(bits, 0, sizeof(bits));
 
     while(fgets(line, sizeof(line), fp)) {
-        for(i = 0; i < 12; i++)
+        for(i = 0; i < INPUT_BITS; i++)
             bits[i] += line[i] - '0';
         count++;
     }
 
-    for(i = 0; i < 12; i++)
-        *((count - bits[i] < bits[i]) ? &gamma : &eps) |= 1 << (11 - i);
+    for(i = 0; i < INPUT_BITS; i++)
+        *((count - bits[i] < bits[i]) ? &gamma : &eps) |= 1 << (INPUT_BITS - 1 - i);
     printf("GAMMA: %u, EPS: %u\n", gamma, eps);
     printf("RES: %u\n", gamma * eps);
 
